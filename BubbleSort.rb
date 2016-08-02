bae561a48b1611b9cc15509714bcd48acccffb6a
hankliu5 <liu19901124@gmail.com>
2016-08-02 10:48:51 -0700

    updated bubblesort method.

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,26 +1,25 @@
 # Code basically taken from Sedgewick, "Algorithms"
 
 def bubble_sort arr
-  movements = 0
+  puts "Original array: #{arr.to_s}"
+  puts "Processing..."
+  swap = 0
   comparisons = 0
-  loop do
-    movements += 1
+  for i in 0...arr.length - 1
     temp_first = arr[0]
-    for i in 1...arr.length
+    for j in 1...arr.length - i
       comparisons += 1
-      if arr[i - 1] > arr[i]
-        movements += 3
-        temp = arr[i]
-        arr[i] = arr[i - 1]
-        arr[i - 1] = temp
+      if arr[j - 1] > arr[j]
+        swap += 1
+        arr[j], arr[j - 1] = arr[j - 1], arr[j]
       end
+      puts arr.to_s
     end
-    comparisons += 1
-    break if temp_first == arr[0]
   end
-  return arr, comparisons, movements
+  return arr, comparisons, swap
 end
 
-array = [3, 2, 1]
+array = [27, 18, 9, 25, 50, 40, 35]
 result = bubble_sort(array)
-puts "result array is #{result[0].to_s}, comparisons: #{result[1]}, movements: #{result[2]}"
+puts
+puts "result array is #{result[0].to_s}, comparisons: #{result[1]}, swaps: #{result[2]}"
